955d12a27aee6489cc648a2d9fe758b82b97a5b4
Alessa L <98236508+alessa-lara@users.noreply.github.com>
2026-05-04 23:08:56 -0300

    [style](queueDynFunctions) h -> hpp and spacing changes

--- lib/queueDynFunctions.hpp
+++ lib/queueDynFunctions.hpp
@@ -1,45 +1,45 @@
-#include "queueDyn.h"
+#include "queueDyn.hpp"
 #include "stackDyn.hpp"
 
 template <typename T>
 void invert(Queue<T>& queue) {
     Stack<T> stack;
 
-    while (! isEmpty(queue))
+    while ( !isEmpty(queue) )
         push(stack, dequeue(queue));
 
-    while (! isEmpty(stack))
+    while ( !isEmpty(stack) )
         enqueue(queue, pop(stack));
 }
 
-template<typename T>
+template <typename T>
 Queue<T> concatenate(Queue<T>& queueFirst, Queue<T>& queueSecond) {
     Queue<T> queueConcat();
 
-    while (! isEmpty(queueFirst))
+    while ( !isEmpty(queueFirst) )
         enqueue(queueConcat, dequeue(queueFirst));
 
-    while (! isEmpty(queueSecond))
+    while ( !isEmpty(queueSecond) )
         enqueue(queueConcat, dequeue(queueSecond));
 
     return queueConcat;
 }
 
-template<typename T>
+template <typename T>
 Queue<T> split(Queue<T> queue, T val) {
     Queue<T> subQueue();
     Queue<T> aux();
 
     bool found = false;
-    while (! isempty(queue) && !found) {
+    while ( !isempty(queue) && !found ) {
         T deqVal = dequeue(queue);
-        if (deqVal == val)
+        if ( deqVal == val )
             break;
 
         enqueue(aux, deqVal);
     }
 
-    while (! isEmpty(queue))
+    while ( !isEmpty(queue) )
         enqueue(subQueue, dequeue(queue));
 
     return subQueue;
