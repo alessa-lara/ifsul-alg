afc0bfdb314ceb435901000c8c632fa18f526f94
Alessa L <98236508+alessa-lara@users.noreply.github.com>
2026-05-04 23:10:58 -0300

    [feat](queue, stack) tests

--- main.cpp
+++ main.cpp
@@ -1,1 +1,81 @@
 // Main file primarily used to test my lib
+
+#include "lib/queueDyn.hpp"
+#include "lib/stackDyn.hpp"
+#include <stdexcept>
+
+using namespace std;
+
+void testQueue() {
+    cout << "=== TEST DYNAMIC QUEUE ===" << endl;
+    Queue<int> q;
+    cout << "isempty: " << isEmpty(q) << endl;
+    enqueue(q, 1);
+    enqueue(q, 2);
+    enqueue(q, 3);
+    enqueue(q, 4);
+    cout << "isempty: " << isEmpty(q) << endl;
+    cout << "peek: " << peek(q) << endl;
+    cout << "show: ";
+    show(q);
+    cout << "search: " << search(q, 1) << endl;
+    dequeue(q);
+    dequeue(q);
+    dequeue(q);
+    dequeue(q);
+    cout << "show: ";
+    show(q);
+
+    try {
+        dequeue(q);
+    } catch ( runtime_error ) {
+        cout << "caught dequeue" << endl;
+    }
+
+    try {
+        peek(q);
+    } catch ( runtime_error ) {
+        cout << "caught peek" << endl;
+    }
+}
+
+void testStack() {
+    cout << "=== TEST DYNAMIC STACK ===" << endl;
+    Stack<int> s;
+    cout << "isempty: " << isEmpty(s) << endl;
+    push(s, 1);
+    push(s, 2);
+    push(s, 3);
+    push(s, 4);
+    cout << "isempty: " << isEmpty(s) << endl;
+    cout << "peek: " << peek(s) << endl;
+    cout << "show: ";
+    show(s);
+    cout << endl
+         << "search: " << search(s, 1) << endl;
+    cout << "pop: " << pop(s) << endl;
+    cout << "pop: " << pop(s) << endl;
+    cout << "pop: " << pop(s) << endl;
+    cout << "pop: " << pop(s) << endl;
+    cout << "show: ";
+    show(s);
+
+    try {
+        pop(s);
+    } catch ( runtime_error ) {
+        cout << endl
+             << "caught dequeue" << endl;
+    }
+
+    try {
+        peek(s);
+    } catch ( runtime_error ) {
+        cout << "caught peek";
+    }
+}
+
+int main() {
+    testQueue();
+    cout << endl;
+    testStack();
+}
