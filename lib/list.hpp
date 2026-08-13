8921be8ca1a80b9f8d90c63f8cad869bb703d9be
Alessa Lara <98236508+alessa-lara@users.noreply.github.com>
2026-08-13 17:35:26 -0300

    [feat] list constructor node

--- lib/list.hpp
+++ lib/list.hpp
@@ -8,6 +8,11 @@ struct Node {
     T data;
     Node* next;
 
+    Node(T data) {
+        this->data = data;
+        this->next = nullptr;
+    }
+
     Node() {
         this->next = nullptr;
     }
@@ -15,7 +20,7 @@ struct Node {
 
 template <typename T>
 bool isEmpty(Node<T>*& linkList) {
-    if (linkList == nullptr)
+    if ( linkList == nullptr )
         return true;
 
     return false;
@@ -25,7 +30,7 @@ template <typename T>
 void deleteList(Node<T>*& linkList) {
     Node<T>* node = linkList;
 
-    while (node != nullptr) {
+    while ( node != nullptr ) {
         Node<T>* aux = node;
         node = node->next;
         delete aux;
@@ -38,42 +43,46 @@ template <typename T>
 void show(Node<T>*& linkList) {
     Node<T>* node = linkList;
 
-    while (node != nullptr) {
+    while ( node != nullptr ) {
         std::cout << node->data;
 
         node = node->next;
 
-        if (node != nullptr)
+        if ( node != nullptr )
             std::cout << ", ";
     }
 }
 
 template <typename T>
-bool insertStart(Node<T>*& linkList, T val) {
-
+bool insertStart(Node<T>** linkList, T val) {
     Node<T>* newNode = new Node<T>;
-    if (newNode == nullptr)
+    if ( newNode == nullptr )
         return false;
 
     newNode->data = val;
-    newNode->next = linkList;
+    newNode->next = *linkList;
     linkList = newNode;
 
     return true;
 }
 
 template <typename T>
-bool insertEnd(Node<T>*& linkList, T val) {
+bool insertEnd(Node<T>** linkList, T val) {
     Node<T>* newNode = new Node<T>;
-    if (newNode == nullptr)
+    if ( newNode == nullptr )
         return false;
 
     newNode->data = val;
     newNode->next = nullptr;
 
+    if ( *linkList == nullptr ) {
+        *linkList = newNode;
+        return true;
+    }
+
     Node<T>* prev = nullptr;
-    Node<T>* node = linkList;
-    while (node != nullptr) {
+    Node<T>* node = *linkList;
+    while ( node != nullptr ) {
         prev = node;
         node = node->next;
     }
@@ -88,16 +97,16 @@ bool removeValue(Node<T>*& linkList, T val) {
     Node<T>* prev = nullptr;
     Node<T>* node = linkList;
 
-    while (node != nullptr && node->data != val) {
+    while ( node != nullptr && node->data != val ) {
         prev = node;
         node = node->next;
     }
 
-    if (node == nullptr)
+    if ( node == nullptr )
         return false;
 
     // val está no inicio da lista
-    if (prev == nullptr)
+    if ( prev == nullptr )
         linkList = node->next;
     else
         prev->next = node->next;
@@ -111,8 +120,8 @@ template <typename T>
 bool search(Node<T>*& linkList, T val) {
     Node<T>* node = linkList;
 
-    while (node != nullptr) {
-        if (node->data == val)
+    while ( node != nullptr ) {
+        if ( node->data == val )
             return true;
 
         node = node->next;
